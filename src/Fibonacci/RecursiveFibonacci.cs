05b40c457a51eaa0e7195b735a2b65d5b3c026f0
Filip Tijan <ftijan@gmail.com>
2021-05-13 20:58:11 +0100

    adding fibonacci solution and tests

---
+++ src/Fibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,21 @@
+﻿namespace Fibonacci
+{
+    /// <summary>
+    /// The recursive fibonacci algorithm calculator.
+    /// </summary>
+    public class RecursiveFibonacci : IFibonacci
+    {
+        /// <inheritdoc/>
+        public long GetNthMember(long n)
+        {
+            ValidationHelper.ThrowOnInvalidFibonacciArgumentValue(n);
+
+            return n switch
+            {
+                1 => 0,
+                2 => 1,
+                _ => GetNthMember(n - 1) + GetNthMember(n - 2)
+            };
+        }
+    }
+}
---
+++ src/Fibonacci/ValidationHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    /// <summary>
+    /// Validation logic helper methods.
+    /// </summary>
+    public static class ValidationHelper
+    {
+        /// <summary>
+        /// Throws <see cref="ArgumentException"/> when the value of <see cref="n"/> is 0 or less.
+        /// </summary>
+        /// <param name="n">The order number of the fibonacci sequence member.</param>
+        public static void ThrowOnInvalidFibonacciArgumentValue(long n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("The value of 'n' should be greater than 0.", nameof(n));
+            }
+        }
+    }
+}
